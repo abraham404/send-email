3e75d2ec2d0903837a313a581028f020cada87e6
abraham <abraham.work7@gmail.com>
2024-09-14 09:34:37 -0700

    first commit

---
+++ send_email.py
@@ -0,0 +1,211 @@
+from flask import Flask, request, jsonify, send_file
+from flask_cors import CORS
+import sqlite3
+import os
+from pathlib import Path
+import re
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+
+app = Flask(__name__)
+CORS(app)
+
+def send_email(subject, body, to_emails, from_email, from_password, files=[]):
+    # Crear el contenedor del mensaje
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = ', '.join(to_emails)
+    msg['Subject'] = subject
+
+    # Agregar el cuerpo del mensaje
+    msg.attach(MIMEText(body, 'plain'))
+
+    # Adjuntar archivos
+    for file in files:
+        with open(file, 'rb') as f:
+            part = MIMEBase('application', 'octet-stream')
+            part.set_payload(f.read())
+            encoders.encode_base64(part)
+            part.add_header('Content-Disposition', f'attachment; filename={file}')
+            msg.attach(part)
+
+    # Iniciar sesión en el servidor y enviar el correo
+    try:
+        server = smtplib.SMTP('smtp-mail.outlook.com', 587)  # Puedes cambiarlo por tu servidor SMTP
+        server.starttls()
+        server.login(from_email, from_password)
+        text = msg.as_string()
+        server.sendmail(from_email, to_emails, text)
+        server.quit()
+        print("Email sent successfully.")
+    except Exception as e:
+        print(f"Error sending email: {e}")
+
+
+
+@app.post('/api/data')
+def sendData(): 
+    
+    data = request.json  # Obtener datos JSON enviados desde el frontend
+    email = data.get('email')
+    password = data.get('password')
+    company = data.get('company')
+    period = data.get('period')
+    year = data.get('year')
+
+    # Especificar el directorio
+    #directorio = Path('C:/Users/Abraham-TI/Desktop/mail')
+    directorio = Path('\\\\192.168.101.108\\Users\\Abraham\\Desktop\\emailAbraham')
+
+    i = 1
+    regex = r'(?:[^_]*_){5}([^_]+)'
+    regex2 = r'(\d{4})_(\d{1,2})'
+    print(f"Email: {email}, Password: {password}, Company: { type(company)}, Period: {period}, Year: {year}")
+    print(type("hola"))
+    
+    try:
+        #Conexión a la base de datos
+        db_conection=sqlite3.connect("C:/sqlite/database_rh.db")
+        cursor=db_conection.cursor()
+
+        # Recorrer los archivos en el directorio
+        for file in os.listdir(directorio):
+            i=i+1
+            if i%2 != 0:
+                full_path = os.path.join(directorio, file)
+                if os.path.isfile(full_path):  # Verificar si es un archivo
+                    with open(full_path, 'r', encoding='utf-8') as f:
+                    
+                        name_ruta = file[0:-4]
+                        period_year = re.search(regex2, name_ruta)
+                    
+                        year_link = period_year.group(1)
+                        period_linl = period_year.group(2)
+
+                        if year_link == year and period_linl == period:
+                            num_employee = re.search(regex, name_ruta)
+                            code_employee = num_employee.group(1)
+                            print(company)
+                            cursor.execute(f"SELECT email, nombre FROM {company} WHERE num_empleado = ?", (code_employee,))
+                            emails_destina=cursor.fetchall()
+                            for email_destina in emails_destina:
+                                try:
+                                    print(email_destina[0])
+                                    print(f'Serie de caracteres extraídos: {code_employee}')
+                                    if __name__ == "__main__":
+                                        subject = "Recibo de nomina de: " + email_destina[1]
+                                        body = "Recibo de nomina"
+                                        to_emails = [email_destina[0]]
+                                        from_email = email
+                                        from_password = password
+                                        files = ["C:/Users/Abraham-TI/Desktop/mail/"+name_ruta+".pdf", "C:/Users/Abraham-TI/Desktop/mail/"+name_ruta+".xml"]  # Lista de archivos a adjuntar
+                                    send_email(subject, body, to_emails, from_email, from_password, files)
+                                     
+                                    print('--- Fin del archivo ---\n')
+                                     # Retorna una respuesta JSON
+                                    #message = {"msg": "¡Correos enviados con exito!"}
+                                    #   return jsonify(message) 
+                                except Exception as ex:
+                                    print(ex)
+                                    message = {"msg": "¡Hubo un problema!"}
+                                    return jsonify(message)
+                    
+    except Exception as ex:
+        print(ex)
+
+        # Asegúrate de que el archivo exista y esté en la carpeta correcta
+ 
+    #return jsonify({"message": "Datos recibidos con éxito"}), 200
+    message = {"msg": "¡Correos enviados con exito!"}
+    return jsonify(message)
+
+
+
+@app.post('/api/employee')
+def add_employee():
+    
+    new_employee = request.json  # Obtener datos JSON enviados desde el frontend
+    number_emplo = new_employee.get('number_emplo')
+    name_emplo = new_employee.get('name_emplo')
+    email_emplo = new_employee.get('email_emplo')
+    company_emplo = new_employee.get('company_emplo')
+    
+    db_conection=sqlite3.connect("C:/sqlite/database_rh.db")
+    cursor=db_conection.cursor()
+    
+    
+    #print("number_emplo, name_emplo, email_emplo, company_emplo")
+    print(f"Numero del empleado: {number_emplo}, nombre: {name_emplo}, Email: { email_emplo }, Empresa: {company_emplo}")
+    #print(request.get_json())
+    
+    # Consulta SQL para insertar los datos
+    query = f'''
+    INSERT INTO {company_emplo} (num_empleado, nombre, email, empresa)
+    VALUES (?, ?, ?, ?)
+    '''
+    # Ejecutar la consulta
+    cursor.execute(query, (number_emplo, name_emplo, email_emplo, company_emplo))
+
+    # Guardar los cambios en la base de datos
+    db_conection.commit()
+
+    # Cerrar la conexión
+    db_conection.close()
+    
+    message = {"msg": "Empleado guardado con éxito"}
+    return jsonify(message)
+    
+
+@app.route('/api/employee/<int:id>/<string:company>', methods=['GET'])
+def get_employee(id,company):
+    
+
+    try:
+        # Conectar a la base de datos
+        db_conection = sqlite3.connect("C:/sqlite/database_rh.db")
+        cursor = db_conection.cursor()
+
+        # Consulta SQL segura con el ID parametrizado
+        query = f'''
+        SELECT num_empleado, nombre, email FROM {company} WHERE num_empleado = ?
+        '''
+
+        # Ejecutar la consulta pasando el ID como parámetro
+        cursor.execute(query, (id,))
+        result = cursor.fetchone()
+
+        # Verificar si se encontró un resultado
+        if result:
+            return jsonify({'status': 'success', 'employee': {
+                'num_employee': result[0],
+                'name': result[1],
+                'email': result[2]
+            }})
+        else:
+            return jsonify({'status': 'error', 'message': 'Empleado no encontrado'}), 404
+
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)}), 500
+
+    finally:
+        db_conection.close()
+
+
+
+@app.get('/')
+def home():
+    return send_file('static/index.html')
+
+
+if __name__== '__main__':
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
+    
+
+
+    
+
---
+++ static/main.js
@@ -0,0 +1,201 @@
+// Seleccionamos el botón de envío
+const submitButton = document.getElementById('submitButton');
+const submitButtonRegister = document.getElementById('submitButton_register');
+const submitButtonGet = document.getElementById('submitButtonSelect');
+const inputsClear = document.querySelectorAll('.clear-input');
+const combobox = document.getElementById('company');
+
+const numberEmploInput = document.getElementById('num_employee_to_register');
+const nameEmploInput = document.getElementById('name_employee');
+const emailEmploInput = document.getElementById('email_employee');
+
+// Añadimos un event listener al botón para capturar el evento de clic
+submitButton.addEventListener('click', async (event) => {
+    event.preventDefault(); // Prevenimos cualquier acción por defecto
+    submitButton.disabled = true;
+    // Obtenemos los valores de los inputs
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('pass').value;
+    const company = document.getElementById('company').value;
+    const period = document.getElementById('period').value;
+    const year = document.getElementById('year').value;
+
+
+    if (validateEmail(email) === 0 || company === 'Selecciona una empresa' || period === '0') {
+        if (validateEmail(email) === 0) {
+            alert('Por favor, introduce una dirección de correo válida.');
+        } else if (company === 'Selecciona una empresa') {
+            alert('Por favor, selecciona una empresa.');
+        } else if (period === '0') {
+            alert('Por favor, selecciona un periodo.');
+        }
+        submitButton.disabled = false;
+
+    } else {
+        const data = {
+            email,
+            password,
+            company,
+            period,
+            year
+        };
+
+
+        try {
+            // Usamos fetch con async/await para enviar los datos al backend
+            const response = await fetch('/api/data', {
+                method: 'POST', // Usamos el método POST
+                headers: {
+                    'Content-Type': 'application/json' // Indicamos que los datos son JSON
+                },
+                body: JSON.stringify(data) // Convertimos el objeto a JSON
+            });
+
+            const result = await response.json();
+            console.log('Success:', result);
+            submitButton.disabled = false;
+            alert(result.msg)
+
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+});
+
+
+submitButtonRegister.addEventListener('click', async (event) => {
+    event.preventDefault(); // Prevenimos cualquier acción por defecto
+
+    // Obtén los elementos del DOM
+    const companyEmploInput = document.getElementById('company');
+
+    // Obtén los valores de los campos
+    const number_emplo = numberEmploInput.value;
+    const name_emplo = nameEmploInput.value;
+    const email_emplo = emailEmploInput.value;
+    const company_emplo = companyEmploInput.value;
+
+    console.log(validateEmail(email_emplo))
+    if (validateEmail(email_emplo) === 0) {
+        alert('Por favor, introduce una dirección de correo válida.');
+    } else {
+
+        //console.log(`${num_employee} ${name_employee} ${email_employee} ${company_employee}`);
+
+        const data_employee = {
+            number_emplo,
+            name_emplo,
+            email_emplo,
+            company_emplo
+        };
+
+
+        try {
+            // Usamos fetch con async/await para enviar los datos al backend
+            const response = await fetch('/api/employee', {
+                method: 'POST', // Usamos el método POST
+                headers: {
+                    'Content-Type': 'application/json' // Indicamos que los datos son JSON
+                },
+                body: JSON.stringify(data_employee) // Convertimos el objeto a JSON
+            });
+
+            console.log(response);  //
+            const result = await response.json();
+            console.log('Success:', result);
+            //submitButton.disabled = false;
+            // Resetea los campos del formulario
+            numberEmploInput.value = "";
+            nameEmploInput.value = "";
+            emailEmploInput.value = "";
+            //companyEmploInput.value = "";
+            alert(result.msg)
+
+        } catch (error) {
+            console.error('Error:', error);
+        }
+
+    }
+
+
+})
+
+
+submitButtonGet.addEventListener('click', async (event) => {
+    event.preventDefault();
+    // Obtener el valor del input de número de empleado
+    const number_emplo = document.getElementById('num_employee').value; // Necesitas el valor, no el elemento
+    const company_emplo = document.getElementById('company').value;
+
+    try {
+        // Usamos fetch con async/await para enviar los datos al backend
+        const response = await fetch(`/api/employee/${number_emplo}/${company_emplo}`, { // Usamos backticks y ${}
+            method: 'GET', // Usamos el método GET
+            headers: {
+                'Content-Type': 'application/json' // No es necesario este encabezado en GET, pero lo mantenemos por compatibilidad
+            }
+        });
+
+        
+        const result = await response.json(); // Procesamos la respuesta como JSON
+        
+        
+        if (result.status === 'success' && result.employee) {
+            const { num_employee, name, email } = result.employee;
+            numberEmploInput.value = num_employee;
+            nameEmploInput.value = name;
+            emailEmploInput.value = email;
+
+        } else {
+            
+            alert('No se encontro el empleado')
+        }
+
+
+    } catch (error) {
+        console.error('Error:', error); // Manejo de errores
+    }
+
+    console.log(number_emplo.value)
+})
+
+
+const clearInput = () => {
+    inputsClear.forEach((element) => {
+        element.value = "";
+    })
+}
+
+
+
+validateEmail = email => {
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (!emailPattern.test(email)) {
+        return 0
+    }
+    else {
+        return 1;
+    }
+
+}
+
+combobox.addEventListener('change', function () {
+
+    if (combobox.value == 'ctBAJA_PACK_SA_LAPQ' || combobox.value == 'ctBAJA_PACK_SA_CULQ' || combobox.value == 'ctECO_BAJA_TOURS_NM') {
+
+        combobox.style.backgroundColor = "#c9f6d6";
+
+    } else if ((combobox.value == 'ctECO_BAJA_TOURS_2020Q' || combobox.value == 'ctTRANSPORTE_ULPZS')) {
+
+        combobox.style.backgroundColor = "#f6c9e9";
+
+    } else {
+        combobox.style.backgroundColor = "#FFF";
+    }
+
+
+
+})
+
